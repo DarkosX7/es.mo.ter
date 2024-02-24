d9dd0e5e3d251ed880c9b68b58fc28f29fef2654
Darkos7 <andreaciardo33@gmail.com>
2024-02-24 19:51:07 +0100

    Prove

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-
 let item = document.querySelector('.nav-hamburger');
 
 item.addEventListener("click", function(){
     document.body.classList.toggle('nav__links-open');
 });
 
-
 import LocomotiveScroll from 'locomotive-scroll';
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
+
 });
+
+alert('wow');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
