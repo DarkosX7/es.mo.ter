7572e488a879f3d4e6011c1b21d35322dc4f69f3
Darkos7 <andreaciardo33@gmail.com>
2024-02-24 16:18:44 +0100

    locomotive-scroll added

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ let item = document.querySelector('.nav-hamburger');
 item.addEventListener("click", function(){
     document.body.classList.toggle('nav__links-open');
 });
+
+
+import LocomotiveScroll from 'locomotive-scroll';
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
